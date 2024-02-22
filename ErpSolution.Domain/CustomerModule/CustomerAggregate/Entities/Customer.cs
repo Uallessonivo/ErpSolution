fac4e89d6a27f9d8e6da06cf54a5bf7fd2a717f1
Uallessonivo <uallessons@gmail.com>
2024-02-22 17:07:53 -0300

    Domain: Implemented setters and constructors to Customer entity and initial validation logic

--- ErpSolution.Domain/CustomerModule/CustomerAggregate/Entities/Customer.cs
+++ ErpSolution.Domain/CustomerModule/CustomerAggregate/Entities/Customer.cs
@@ -6,17 +6,100 @@ namespace ErpSolution.Domain.CustomerModule.CustomerAggregate.Entities
 {
     public class Customer : BaseEntity
     {
-        public int Code { get; set; }
-        public string CNPJ { get; set; } 
-        public string CPF { get; set; } 
-        public string Name { get; set; } 
-        public string Email { get; set; }
-        public Status Status { get; set; } = Status.Active;
-        public Address Address { get; set; }
-        public Contact Contact { get; set; } 
-        public Category Category { get; set; } 
-        public List<Seller> Sellers { get; set; }
-        public List<History> History { get; set; } 
-        public FinancialInformation FinancialInformation { get; set; } 
+        private string Cnpj { get; set; }
+        private string Cpf { get; set; }
+        private string Email { get; set; }
+        private string Name { get; set; }
+        private Status CustomerStatus { get; set; } = Status.Active;
+        private Address Address { get; set; }
+        private Contact Contact { get; set; }
+        private Category Category { get; set; }
+        private IReadOnlyList<Seller> Sellers { get; set; }
+        private IReadOnlyList<History> History { get; set; }
+        private FinancialInformation FinancialInformation { get; set; }
+
+        public Customer(string cnpj, string cpf, string email, string name, Address address, Contact contact,
+            Category category)
+        {
+            Cnpj = cnpj;
+            Cpf = cpf;
+            Email = email;
+            Name = name;
+            Address = address;
+            Contact = contact;
+            Category = category;
+            Sellers = new List<Seller>().AsReadOnly();
+            History = new List<History>().AsReadOnly();
+            FinancialInformation = new FinancialInformation();
+        }
+
+        public Customer AddSeller(Seller seller)
+        {
+            // Use validation
+            return new Customer(Cnpj, Cpf, Email, Name, Address, Contact, Category)
+            {
+                Sellers = Sellers.Concat(new[] { seller }).ToList().AsReadOnly()
+            };
+        }
+
+        public Customer RemoveSeller(Seller seller)
+        {
+            // Use validation
+            return new Customer(Cnpj, Cpf, Email, Name, Address, Contact, Category)
+            {
+                Sellers = Sellers.Where(x => x.Id != seller.Id).ToList().AsReadOnly()
+            };
+        }
+
+        public Customer AddHistory(History history)
+        {
+            // Use validation
+            return new Customer(Cnpj, Cpf, Email, Name, Address, Contact, Category)
+            {
+                History = History.Concat(new[] { history }).ToList().AsReadOnly()
+            };
+        }
+
+        public Customer UpdateFinancialInformation(FinancialInformation financialInformation)
+        {
+            // Use validation
+            return new Customer(Cnpj, Cpf, Email, Name, Address, Contact, Category)
+            {
+                FinancialInformation = financialInformation
+            };
+        }
+
+        public Customer UpdateCategory(Category category)
+        {
+            // Use validation
+            return new Customer(Cnpj, Cpf, Email, Name, Address, Contact, category);
+        }
+
+        public Customer UpdateContact(Contact contact)
+        {
+            // Use validation
+            return new Customer(Cnpj, Cpf, Email, Name, Address, contact, Category);
+        }
+
+        public Customer UpdateAddress(Address address)
+        {
+            // Use validation
+            return new Customer(Cnpj, Cpf, Email, Name, address, Contact, Category);
+        }
+
+        public Customer UpdateName(string name)
+        {
+            // Use validation
+            return new Customer(Cnpj, Cpf, Email, name, Address, Contact, Category);
+        }
+
+        public Customer UpdateStatus(Status status)
+        {
+            // Use validation
+            return new Customer(Cnpj, Cpf, Email, Name, Address, Contact, Category)
+            {
+                CustomerStatus = status
+            };
+        }
     }
 }
---
+++ ErpSolution.Domain/CustomerModule/CustomerAggregate/Validations/CustomerValidations.cs
@@ -0,0 +1,16 @@
+namespace ErpSolution.Domain.CustomerModule.CustomerAggregate.Validations;
+
+public class CustomerValidations
+{
+    // public static void ValidateCnpj(string cnpj)
+    // public static void ValidateCpf(string cpf)
+    // public static void ValidateEmail(string email)
+    // public static void ValidateName(string name)
+    // public static void ValidateAddress(Address address)
+    // public static void ValidateContact(Contact contact)
+    // public static void ValidateCategory(Category category)
+    // public static void ValidateSellers(List<Seller> sellers)
+    // public static void ValidateHistory(List<History> history)
+    // public static void ValidateFinancialInformation(FinancialInformation financialInformation)
+    // public static void ValidateCustomerStatus(Status customerStatus)
+}
